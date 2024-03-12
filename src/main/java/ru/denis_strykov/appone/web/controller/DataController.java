4d1a5efd4d62628570c3901d604a7aa1deb2a1aa
Denis Strukov <d.strukov@astondevs.ru>
2024-03-12 14:30:44 +0300

    Создал DataController c endpoint (/api/v1/data/send) для отправки сообщения в Kafka

---
+++ src/main/java/ru/denis_strykov/appone/web/controller/DataController.java
@@ -0,0 +1,29 @@
+package ru.denis_strykov.appone.web.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.denis_strykov.appone.model.Data;
+import ru.denis_strykov.appone.web.dto.DataDto;
+import ru.denis_strykov.appone.web.mapper.DataMapper;
+
+@RestController
+@RequestMapping("api/v1/data")
+@RequiredArgsConstructor
+public class DataController {
+
+    private final KafkaDataService kafkaDataService;
+
+    private final DataMapper dataMapper;
+
+    @PostMapping("/send")
+    public void send(
+            @RequestBody DataDto dto
+            ) {
+        Data data = dataMapper.toEntity(dto);
+        kafkaDataService.send(data);
+    }
+
+}
