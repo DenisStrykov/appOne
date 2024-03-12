912546a9145b56c3e7de845884aca1bf243eb150
Denis Strukov <d.strukov@astondevs.ru>
2024-03-12 12:11:34 +0300

    git init

---
+++ src/main/java/ru/denis_strykov/appone/AppOneApplication.java
@@ -0,0 +1,13 @@
+package ru.denis_strykov.appone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppOneApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppOneApplication.class, args);
+    }
+
+}
